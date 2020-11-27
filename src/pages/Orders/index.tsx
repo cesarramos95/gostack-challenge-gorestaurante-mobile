0562eeacc341d4f08b24cd9e6864873bef325e2f
cesarramos95 <cesarramos.aug@gmail.com>
2020-11-27 08:50:42 -0300

    Search functions implemented for Dashboard, Orders and Favorites pages

--- src/pages/Orders/index.tsx
+++ src/pages/Orders/index.tsx
@@ -33,6 +33,14 @@ const Orders: React.FC = () => {
   useEffect(() => {
     async function loadOrders(): Promise<void> {
       // Load orders from API
+      const response = await api.get('/orders');
+
+      const formattedFoodPrice = response.data.map((food: Food) => ({
+        ...food,
+        formattedPrice: formatValue(food.price),
+      }));
+
+      setOrders(formattedFoodPrice);
     }
 
     loadOrders();
